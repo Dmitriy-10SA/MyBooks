933299fe02cea7dd8f3cc0427bf3823300b34de0
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-03-20 22:28:55 +0400

    Tired...

--- app/src/main/java/com/andef/mybooks/presentation/find/FindScreen.kt
+++ app/src/main/java/com/andef/mybooks/presentation/find/FindScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
